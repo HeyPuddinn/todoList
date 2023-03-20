c7409758182d79b7cfee684f35c92f021e1eb4d2
thuc.nguyen <thuc.nguyen@9thwonder.com>
2023-03-20 11:47:25 +0700

    first-commit

---
+++ component/App.js
@@ -0,0 +1,19 @@
+import html from "../core.js";
+import Header from "../component/header.js";
+import { connect } from "../store.js";
+import TodoList from "../component/TodoList.js";
+import Footer from "../component/Footer.js";
+
+const connector = connect()
+
+function App({ todos }){
+  return html`
+    <section class="todoapp">
+      ${Header()}
+      ${todos.length > 0 && TodoList()}
+      ${todos.length > 0 && Footer()}
+    </section>
+    `
+}
+
+export default connector(App)
---
+++ component/TodoList.js
@@ -0,0 +1,30 @@
+import html from "../core.js";
+import TodoItem from "../component/TodoItem.js";
+import { connect } from "../store.js";
+
+const connector = connect()
+
+function TodoList({ todos, filters, filter, editIndex }){
+  return html`
+    <section class="main">
+      <input
+        id="toggle-all"
+        class="toggle-all"
+        type="checkbox"
+        onchange="dispatch('toggleAll',this.checked)"
+        ${todos.every(filters.completed) && 'checked'}
+      >
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        ${todos
+          .filter(filters[filter])
+          .map((todo, index) => 
+            TodoItem({todo, index, editIndex})
+          )}
+        
+      </ul>
+    </section>
+  `
+}
+
+export default connector(TodoList)
